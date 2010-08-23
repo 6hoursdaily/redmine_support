3e6b2c1aefbc04f5677c96a03c4a539a76a31ab2
Nick Bolton <nick.bolton.uk@gmail.com>
2010-08-23 23:15:54 +0100

    issues page help message

--- init.rb
+++ init.rb
@@ -12,3 +12,21 @@ Redmine::Plugin.register :redmine_support do
   end
   
 end
+
+class SupportViewer < Redmine::Hook::ViewListener
+  def view_layouts_base_body_bottom(context = { })
+    if context[:controller].class.name == 'IssuesController'
+      image = image_tag('help.png',
+                        :style => 'margin-bottom: -0.3em;').gsub('"', '\"')
+      
+      # TODO: make this html a plugin setting
+      return "
+<script type=\"text/javascript\">
+  var elems = $$('#content h2')
+  if ((elems != null) && (elems.length > 0))
+    elems[0].insert({after: \"<p>#{image} New users cannot create issues. Please see the <a href=\\\"../tabs/support\\\">support</a> page for more info.</p>\"})
+</script>
+"
+    end
+  end
+end
